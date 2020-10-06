7372bc5b009127591f81470ef72fc3edc1b53d3e
Niki Minev <niki.minev.a24@learn.telerikacademy.com>
2020-10-06 18:35:29 +0300

    invalidate token on logout

--- src/auth/auth-middleware.js
+++ src/auth/auth-middleware.js
@@ -17,6 +17,16 @@ const authMiddleware = passport.authenticate('jwt', { session: false });
 const roleMiddleware = roleName => {
     return (req, res, next) => {
         if (req.user && req.user.role === roleName) {
+            const auth = (req.headers.authorization).split(' ');
+            const token = auth[1];
+            const rawData = fs.readFileSync(LOG_FILE_PATH);
+            const parsedDataArray = Array.from(JSON.parse(rawData));
+            const set = new Set(parsedDataArray);
+
+            if (set.has(token)) {
+                return res.status(400).send({ message: 'You must be logged in to see this content!'});
+            }
+
             next();
         } else {
             res.status(403).send({
@@ -27,18 +37,21 @@ const roleMiddleware = roleName => {
 };
 
 const blacklistTokenMiddleware = () => {
-    return (req, res, next) => {
+    return async (req, res, next) => {
         const auth = (req.headers.authorization).split(' ');
         const token = auth[1];
         const rawData = fs.readFileSync(LOG_FILE_PATH);
-        const parsedDataArray = JSON.parse(rawData);
+        const parsedDataArray = Array.from(JSON.parse(rawData));
         const set = new Set(parsedDataArray);
+
         if (!set.has(token)) {
             set.add(token);
         }
-        const newStringJson = JSON.stringify(parsedDataArray);
-        fs.writeFileSync(LOG_FILE_PATH, newStringJson);
 
+        const newArray = Array.from(set);
+        fs.writeFileSync(LOG_FILE_PATH, JSON.stringify(newArray));
+
+        next();
     };
 };
 
