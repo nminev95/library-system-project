b240de17f3a3bf9bcda168deb930185f1cc4585b
Niki Minev <niki.minev.a24@learn.telerikacademy.com>
2020-10-14 16:30:03 +0300

    added router - home/signin/register

--- client/src/App.js
+++ client/src/App.js
@@ -1,31 +1,22 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import NavBar from './Components/NavBar';
-import LoginForm from './Components/LoginForm';
-import Content from './Components/Content';
-import TextImageRow from './Components/TextImageRow';
 import './App.css';
-import books from './Closed_Book_Icon.svg'
-import returnBook from './aha.png'
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import HomePage from './Components/Homepage';
+import LoginForm from './Components/LoginForm';
 
 function App() {
   return (
-    <div className="body">
-      <NavBar sticky="top" />
-      <Content />
-      <TextImageRow icon={books}>
-        <h1>Huge variety.</h1>
-        <p>Many famous and best selling books from all over the world.</p>
-        <p>Choose from fictional, horror, biographical, novels, scientific, poetry and others.</p>
-        <p>All of world's top renowned authors in one library.</p>
-      </TextImageRow>
-      <TextImageRow icon={returnBook}>
-        <h1>Borrow and return anytime.</h1>
-        <p>Unlimited borrow capacity. Any book, any time, for any period.</p>
-        <p>Over 10000 books available and waiting to be borrowed.</p>
-        <p>Game system - gain points for every book read and returned and earn rewards.</p>
-      </TextImageRow>
-    </div>
+
+    <Router>
+      <div className="body">
+        <Switch>
+          <Route path='/' exact component={HomePage} />
+          <Route path='/auth/signin' component={LoginForm} />
+          <Route path='/users' component={LoginForm} />
+        </Switch>
+      </div>
+    </Router>
 
   );
 }
---
+++ client/src/Components/Homepage.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import NavBar from './NavBar';
+import LoginForm from './LoginForm';
+import Content from './Content';
+import TextImageRow from './TextImageRow';
+import books from '../Closed_Book_Icon.svg';
+import returnBook from '../aha.png';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import './Homepage.css';
+
+const HomePage = () => {
+  return (
+    
+
+    <div className="body">
+      <NavBar sticky="top" />
+      <Content />
+      <TextImageRow id="1" icon={books}>
+        <h1>Huge variety.</h1>
+        <p>Many famous and best selling books from all over the world.</p>
+        <p>Choose from fictional, horror, biographical, novels, scientific, poetry and others.</p>
+        <p>All of world's top renowned authors in one library.</p>
+      </TextImageRow>
+      <TextImageRow id="2" icon={returnBook}>
+        <h1>Borrow and return anytime.</h1>
+        <p>Unlimited borrow capacity. Any book, any time, for any period.</p>
+        <p>Over 10000 books available and waiting to be borrowed.</p>
+        <p>Game system - gain points for every book read and returned and earn rewards.</p>
+      </TextImageRow>
+      <TextImageRow id="3" icon={returnBook}>
+        <h1>Borrow and return anytime.</h1>
+        <p>Unlimited borrow capacity. Any book, any time, for any period.</p>
+        <p>Over 10000 books available and waiting to be borrowed.</p>
+        <p>Game system - gain points for every book read and returned and earn rewards.</p>
+      </TextImageRow>
+    </div>
+
+  );
+}
+
+export default HomePage;
