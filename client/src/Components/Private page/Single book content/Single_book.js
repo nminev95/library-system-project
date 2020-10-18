ff48c03319a07fee0c3bc97b7638551dd0dba985
Mariya Velikova <m.velikovaa@gmail.com>
2020-10-18 20:34:40 +0300

    Single book vis edited

--- client/src/Components/Private page/Single book content/Single_book.js
+++ client/src/Components/Private page/Single book content/Single_book.js
@@ -1,46 +1,46 @@
 import React from 'react';
 import './Single_book.css';
 import 'mdbreact/dist/css/mdb.css'
-import {MDBBtn} from 'mdbreact';
+import { MDBBtn } from 'mdbreact';
 
 
 const SingleBook = ({ book }) => {
 
     return (
-        <div className="single-book-main">
-            <div id="book-details" className="container my-5 z-depth-1">
+
+        <div id="book-details" className="container my-5 z-depth-1">
 
 
-                <section className="dark-grey-text">
+            <section className="dark-grey-text">
 
-                    <div class="row pr-lg-5">
-                        <div class="col-md-7 mb-4">
+                <div class="row pr-lg-5 d-flex ">
+                    <div class="col-md-4 justify-content-center">
 
-                            <div class="view d-flex justify-content-center border border-light p-5">
-                                <img src={book.Cover} class=" h-100 d-inline-block w-50 p-3" alt="smaple image" />
-                            </div>
-                            <div class="text-center" >
-                            </div>
+                        <img src={book.Cover} class="w-100 p-4 justify-content-center" alt="smaple image" />
 
+
+                    </div>
+                    <div class="col-md-8 p-4 justify-content-center">
+
+                        <div class="p-5 ">
+                            <h3 class="font-weight-bold text-center">{book.Title}</h3>
                         </div>
-                        <div class="col-md-5 d-flex align-items-center">
-                            <div>
 
-                                <h3 class="font-weight-bold mb-4 text-center">{book.Title}</h3>
+                        <div class="p-4">
+                            <p className=" w-auto text-center text-justify " >{book.Description}</p>
+                        </div>
 
-                                <p class="text-center" class="text-justify">{book.Description}</p>
-
-                            <div id="buttons" class="text-center">
+                        <div id="buttons" class="text-center p-5">
                             <MDBBtn id="main-button"  > Borrow </MDBBtn>
                             <MDBBtn id="main-button"  > Reviews </MDBBtn>
-                            </div>
-                            </div>
                         </div>
                     </div>
+                </div>
 
-                </section>
-            </div>
+
+            </section>
         </div>
+
     )
 };
 
