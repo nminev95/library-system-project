148d519b8e93d41cc1c053461b66215181874e90
Niki Minev <niki.minev.a24@learn.telerikacademy.com>
2020-10-19 17:00:33 +0300

    merge//ban pop up

--- client/src/Components/Admin panel/AdminUsers/AdminUsers.js
+++ client/src/Components/Admin panel/AdminUsers/AdminUsers.js
@@ -1,10 +1,12 @@
 import React, { useEffect, useState } from 'react';
-import { MDBBtn, MDBTable, MDBTableBody, MDBTableHead } from 'mdbreact';
+import { MDBBtn, MDBInput, MDBTable, MDBTableBody, MDBTableHead } from 'mdbreact';
 import UserTableRow from './UserTableRow/UserTableRow';
+import BanDeletePopUp from './BanDeletePopUp/BanDeletePopUp';
 
 const AdminUsers = () => {
 
     const [tableContent, setTableContent] = useState([]);
+    const [isHidden, setIsHidden] = useState(true)
 
     const columns = [
         {
@@ -43,6 +45,8 @@ const AdminUsers = () => {
 
     return (
         <MDBTable style={{ margin: "40px" }} btn responsiveMd>
+            <TextareaPage/>
+            <BanDeletePopUp/>
             <MDBTableHead columns={columns} />
             <MDBTableBody>
             {tableContent.map((record) => <UserTableRow content={record}/>)}
@@ -53,3 +57,9 @@ const AdminUsers = () => {
 }
 
 export default AdminUsers;
+
+const TextareaPage = () => {
+    return (
+        <MDBInput type="textarea" label="Material textarea" rows="5" />
+    )
+}
---
+++ client/src/Components/Admin panel/AdminUsers/BanDeletePopUp/BanDeletePopUp.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { MDBNavbar, MDBNavbarBrand, MDBNavbarNav, MDBNavbarToggler, MDBCollapse, MDBNavItem, MDBNavLink, MDBIcon, MDBBtn, MDBContainer, MDBRow } from 'mdbreact';
+
+const BanDeletePopUp = () => {
+    return (
+        <MDBContainer>
+            <MDBRow>
+            <p>why</p>
+            </MDBRow>
+            <MDBRow>
+            <p>when</p>
+            </MDBRow>
+            <MDBRow>    
+            </MDBRow>
+        </MDBContainer>
+    )
+}
+
+export default BanDeletePopUp;
