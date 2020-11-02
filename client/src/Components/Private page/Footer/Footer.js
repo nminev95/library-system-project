f4951908c8b74ee73e3dad042dbe432e1adb74fc
Mariya Velikova <m.velikovaa@gmail.com>
2020-11-02 08:30:47 +0200

    footer changes

--- client/src/Components/Private page/Footer/Footer.js
+++ client/src/Components/Private page/Footer/Footer.js
@@ -10,64 +10,59 @@ const Footer = () => {
   return (
     <>
       {path.includes('admin') || !user ?
-        (<MDBFooter style={{display:"none"}}/>) 
+        (<MDBFooter style={{ display: "none" }} />)
         : (
-        <MDBFooter id="footerDiv" color="grey darken-3" className="font-small lighten-3 pt-4 ">
-          <MDBContainer className="text-center text-md-left">
-            <MDBRow className="my-4">
-              <MDBCol md="4" lg="4">
-                <h5 className="text-uppercase mb-4 font-weight-bold">
-                  Library system
-            </h5>
-                <p>
-                  .
-            </p>
-              </MDBCol>
-              <hr className="clearfix w-100 d-md-none" />
-              <MDBCol md="2" lg="2" className="ml-auto">
-                <h5 className="text-uppercase mb-4 font-weight-bold">About</h5>
-                <ul className="list-unstyled">
+          <MDBFooter id="footerDiv" color="grey darken-3" className="font-small lighten-3 pt-4 text-md-left">
+            <MDBContainer className="text-right text-md-left pl-20px">
+              <MDBRow className="my-4">
+                <MDBCol md="4" lg="3">
+                </MDBCol>
+                <hr className="clearfix w-100 d-md-none" />
+                <MDBCol md="4" lg="3">
+                  <h5 className="text-uppercase mb-4 font-weight-bold">About</h5>
+                  <ul className="list-unstyled">
+                    <p>
+                      <a href="#!">Home</a>
+                    </p>
+                    <p>
+                      <a href="#!">Browse books</a>
+                    </p>
+                  </ul>
+                </MDBCol>
+                <hr className="clearfix w-100 d-md-none" />
+                <MDBCol md="4" lg="3">
+                  <h5 className="text-uppercase mb-4 font-weight-bold">Address</h5>
                   <p>
-                    <a href="#!">Home</a>
+                    <i className="fa fa-home mr-3" /> Sofia, Bulgaria
+            </p>
+                  <p>
+                    <i className="fa fa-envelope mr-3" /> readme@gmail.com
+            </p>
+                </MDBCol>
+                <hr className="clearfix w-100 d-md-none" />
+                <MDBCol md="4" lg="3" >
+
+                  <h5 className="text-uppercase mb-4 font-weight-bold">
+                    System 
+                  </h5>
+                  <p>
+                    Online library
                   </p>
                   <p>
-                    <a href="#!">Browse books</a>
+                    Telerik Academy project
                   </p>
-                </ul>
-              </MDBCol>
-              <hr className="clearfix w-100 d-md-none" />
-              <MDBCol md="5" lg="3">
-                <h5 className="text-uppercase mb-4 font-weight-bold">Address</h5>
-                <p>
-                  <i className="fa fa-home mr-3" /> Sofia, Bulgaria
-            </p>
-                <p>
-                  <i className="fa fa-envelope mr-3" /> info@example.com
-            </p>
-              </MDBCol>
-              <hr className="clearfix w-100 d-md-none" />
-              <MDBCol md="2" lg="2" className="text-center">
-                <h5 className="text-uppercase mb-4 font-weight-bold">
-                  Follow us
-            </h5>
-                <div className="mt-2 ">
-                  <a type="button" className="btn-floating btn-small btn-fb" style={{ marginRight: "20px" }}>
-                    <i className="fab fa-facebook fa-3x" />
-                  </a>
-                  <a type="button" className="btn-floating btn-small btn-tw">
-                    <i className="fab fa-instagram fa-3x" />
-                  </a>
-                </div>
-              </MDBCol>
-              <hr className="clearfix w-100 d-md-none" />
-            </MDBRow>
-          </MDBContainer>
-          <div className="footer-copyright text-center py-3">
-            <MDBContainer fluid>
-              &copy; {new Date().getFullYear()} Copyright: Team 5
+
+
+                </MDBCol>
+                <hr className="clearfix w-100 d-md-none" />
+              </MDBRow>
+            </MDBContainer>
+            <div className="footer-copyright text-center py-3 ml-4">
+              <MDBContainer fluid>
+                &copy; {new Date().getFullYear()} Copyright: Team 5
         </MDBContainer>
-          </div>
-        </MDBFooter>
+            </div>
+          </MDBFooter>
         )}
     </>
   );
